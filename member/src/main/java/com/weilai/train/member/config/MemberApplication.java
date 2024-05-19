1fe09e497eebb9620163237108d1ec7fc8fbcdb7
miku-wwl <569144003@qq.com>
2024-05-19 22:13:46 +0800

    feat:ch4-5 修改启动日志，增加/member

--- member/src/main/java/com/weilai/train/member/config/MemberApplication.java
+++ member/src/main/java/com/weilai/train/member/config/MemberApplication.java
@@ -1,15 +1,23 @@
 package com.weilai.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.weilai")
 public class MemberApplication {
 
+    private static final Logger Log = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        Log.info("启动成功");
+        Log.info("测试地址: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
     }
 
 }
