360099337bf76694adb025d129adf77bef170cd8
miku-wwl <569144003@qq.com>
2024-05-22 23:07:18 +0800

    feat:ch4-15 实现一个简单的注册接口。

--- member/src/main/java/com/weilai/train/member/service/MemberService.java
+++ member/src/main/java/com/weilai/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.weilai.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.weilai.train.member.domain.Member;
+import com.weilai.train.member.domain.MemberExample;
 import com.weilai.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -11,5 +16,21 @@ public class MemberService {
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(list)){
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
+
     }
 }
